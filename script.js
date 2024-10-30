9d861d8872d00b173552793cb2ad13cdb3f1ec91
MariamTurdziladze <mariamt789@gmail.com>
2024-10-30 22:56:47 +0400

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+let Name = "Mariam";
+let Surname = "Turdziladze";
+let Age = 26;
+let email = "mt@gmail.com";
+let tel = 557595874;
+let isAdmin = true;
+
+
+
+console.log("Her name is", Name, "Her surname is", Surname, "Her age is", Age, 'color:red', "Her e-mail is", email, "Her phone number is", tel, "She is", isAdmin);
+
+
+console.log("%c true", "color:yellow");
+console.log("%c 557595874", "color:red");
+
